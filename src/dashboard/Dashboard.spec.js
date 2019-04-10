c3b061643baff70766035a5c0fe5d7457c989dc4
Julian Moreno <julmo611@gmail.com>
2019-04-10 15:06:06 -0700

    MVP

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,31 @@
 // Test away
+import React from 'react';
+import Dashboard from './Dashboard';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+afterEach(cleanup);
+
+describe('<Dashboard />', () => {
+	it(' toggle open and closed', () => {
+		const { getByTestId } = render(<Dashboard locked={false} closed={false} />);
+
+		const toggleClosedBtn = getByTestId('toggle-closed-btn');
+		const testingGateClosed = getByTestId('testingGateClosed');
+
+		fireEvent.click(toggleClosedBtn);
+		expect(toggleClosedBtn).toHaveTextContent('Open Gate');
+		expect(testingGateClosed).toHaveTextContent('Closed');
+	});
+
+	it('toggle unlocked and locked', () => {
+		const { getByTestId } = render(<Dashboard locked={true} closed={true} />);
+
+		const toggleLockedBtn = getByTestId('toggle-locked-btn');
+		const testingGateLocked = getByTestId('testingGateLocked');
+
+		fireEvent.click(toggleLockedBtn);
+		expect(toggleLockedBtn).toHaveTextContent('Lock Gate');
+		expect(testingGateLocked).toHaveTextContent('Unlocked');
+	});
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,60 @@
 // Test away!
+import React from 'react';
+import { render, cleanup } from 'react-testing-library';
+import Display from './Display';
+import 'jest-dom/extend-expect';
+
+afterEach(cleanup);
+
+describe('<Display />', () => {
+	describe('Updates text', () => {
+		it('Open when gate closed props is false', () => {
+			const { getByTestId } = render(<Display closed={false} />);
+			const testingGateClosed = getByTestId('testingGateClosed');
+			expect(testingGateClosed).toHaveTextContent('Open');
+		});
+		it('closed when gate closed props is true', () => {
+			const { getByTestId } = render(<Display closed={true} />);
+			const testingGateClosed = getByTestId('testingGateClosed');
+			expect(testingGateClosed).toHaveTextContent('Closed');
+		});
+
+		it('Unlocked when locked props is false', () => {
+			const { getByTestId } = render(<Display locked={false} />);
+			const testingGateLocked = getByTestId('testingGateLocked');
+			expect(testingGateLocked).toHaveTextContent('Unlocked');
+		});
+
+		it('Locked when locked props is true', () => {
+			const { getByTestId } = render(<Display locked={true} />);
+			const testingGateLocked = getByTestId('testingGateLocked');
+			expect(testingGateLocked).toHaveTextContent('Locked');
+		});
+	});
+
+	describe('Updates styling', () => {
+		it('green-led class when gate is open', () => {
+			const { getByTestId } = render(<Display closed={false} />);
+			const testingGateClosed = getByTestId('testingGateClosed');
+			expect(testingGateClosed).toHaveClass('green-led');
+		});
+
+		it('red-led class when gate is closed', () => {
+			const { getByTestId } = render(<Display closed={true} />);
+			const testingGateClosed = getByTestId('testingGateClosed');
+			expect(testingGateClosed).toHaveClass('red-led');
+		});
+
+		it('green-led class when gate is unlocked', () => {
+			const { getByTestId } = render(<Display locked={false} />);
+			const testingGateLocked = getByTestId('testingGateLocked');
+			expect(testingGateLocked).toHaveClass('green-led');
+		});
+
+		it('red-led class when gate is locked', () => {
+			const { getByTestId } = render(<Display locked={true} />);
+			const testingGateLocked = getByTestId('testingGateLocked');
+			expect(testingGateLocked).toHaveClass('red-led');
+		});
+	});
+});
